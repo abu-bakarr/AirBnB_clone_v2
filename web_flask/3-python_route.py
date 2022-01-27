ebf254903f3648ba0461589143759ba69862c6d0
abu-bakarr <abangura14@yahoo.com>
2022-01-27 19:08:27 +0000

    Add file 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
@@ -21,6 +21,14 @@ def c_text(text):
     """c/text route"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """python/text route"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 if __name__ == '__main__':
     app.run()
