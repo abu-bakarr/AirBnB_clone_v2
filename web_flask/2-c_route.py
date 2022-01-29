1268af56c80c836fd36d55d2403c3eb3e15c8cd8
abu-bakarr <abangura14@yahoo.com>
2022-01-29 18:32:05 +0000

    Add file 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+""" Starts Flask. """
 from flask import Flask
-
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    """ DISPLAY Hello HBNB!!! """
-    return ("Hello HBNB!")
+def hello_HBNB():
+    """ Hello HBNB!!! """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
-    """ DISPLAY HBNB!!! """
-    return ("HBNB")
+    """ HBNB!!! """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """c/text route"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run()
