f893a873c536bfbb7603bce88134b01a84e637a8
abu-bakarr <abangura14@yahoo.com>
2022-01-29 18:50:47 +0000

    Add file 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,57 +1,54 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask import render_template
+""" Starts Flask. """
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ DISPLAY Hello HBNB!!! """
-    return ("Hello HBNB!")
+def hello_HBNB():
+    """ Hello HBNB!!! """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
-    """ DISPLAY HBNB!!! """
-    return ("HBNB")
+    """ HBNB!!! """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """c/text route"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """python/text route"""
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<phrase>', strict_slashes=False)
+def python(phrase='is cool'):
+    """ Python + phrases!!!(is cool by default) """
+    return 'Python {}'.format(phrase.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
-    """number/n route"""
-    return "{} is a number".format(n)
+@app.route('/number/<int:num>', strict_slashes=False)
+def number(num):
+    """ It's a number! """
+    if type(num) == int:
+        return ("{} is a number".format(num))
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_html(n):
+@app.route('/number_template/<int:num>', strict_slashes=False)
+def display_html(num):
     """ Display a HTML page. """
-    if type(n) == int:
-        return render_template('5-number.html', num=n)
+    if type(num) == int:
+        return render_template('5-number.html', n=num)
 
 
-@app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even_HBNB(n):
-    if n % 2 == 0:
-        val = "even"
-    else:
-        val = "odd"
-    return render_template('6-number_odd_or_even.html', num=n, val=val)
+@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
+def odd_or_even(num):
+    """ Odd od Even Number """
+    if type(num) == int:
+        return render_template('6-number_odd_or_even.html', n=num)
 
 
 if __name__ == '__main__':
