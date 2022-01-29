860b4aeea3c630afb2e055644d41cbbeb3814cfe
abu-bakarr <abangura14@yahoo.com>
2022-01-29 18:35:34 +0000

    Add file 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
+""" Starts Flask. """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ DISPLAY Hello HBNB!!! """
+def hello_HBNB():
+    """ Hello HBNB!!! """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
-    """ DISPLAY HBNB!!! """
+    """ HBNB!!! """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """c/text route"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """python/text route"""
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<phrase>', strict_slashes=False)
+def python(phrase='is cool'):
+    """ Python + phrases!!!(is cool by default) """
+    return 'Python {}'.format(phrase.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run()
