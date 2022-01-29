c44d962077fb791bb230d9d2f1dd51111164ddb7
abu-bakarr <abangura14@yahoo.com>
2022-01-29 18:30:14 +0000

    Add file 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def index():
-    """Main page rout"""
-    return 'Hello HBNB!'
+def hello():
+    """ DISPLAY Hello HBNB!!! """
+    return ("Hello HBNB!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run()
