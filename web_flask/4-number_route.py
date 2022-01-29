bdaa96ee61195a5b2519dfcc42d3a91f7b7a1ecf
abu-bakarr <abangura14@yahoo.com>
2022-01-29 18:40:47 +0000

    Add file 3

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+""" Starts Flask. """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ DISPLAY Hello HBNB!!! """
-    return ("Hello HBNB!")
+def hello_HBNB():
+    """ Hello HBNB!!! """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
-    """ DISPLAY HBNB!!! """
-    return ("HBNB")
+    """ HBNB!!! """
+    return "HBNB"
 
 
 @app.route('/c/<phrase>', strict_slashes=False)
