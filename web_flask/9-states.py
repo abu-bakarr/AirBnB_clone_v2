db3b8dbd9e9d2abaff2e662b7892b7127484b701
abu-bakarr <abangura14@yahoo.com>
2022-01-30 01:12:59 +0000

    Add file 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,60 +1,33 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage, State
-from flask import render_template
-
+""" Starts Flask. """
+from flask import Flask, render_template
+from models import storage
+from models import State, City
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exceptions):
+def close_storage(self):
+    """ Remove the current SQLAlchemy Session. """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    state_obj = storage.all("State")
-    states = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    return render_template("7-states_list.html", states=states)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<_id>', strict_slashes=False)
+def cities_by_states(_id=None):
+    """ Cities inside each state. """
+    states = list(storage.all('State').values())
+    states.sort(key=lambda state: state.name)
+    cities = list(storage.all('City').values())
+    cities.sort(key=lambda city: city.name)
+
+    state_name = None
+    for state in states:
+        if state.id == _id:
+            state_name = state.name
+    return render_template('9-states.html', states=states, cities=cities,
+                           _id=_id, state_name=state_name)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-    return render_template("8-cities_by_states.html",
-                           states=states,
-                           cities=cities)
-
-
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def show_states(id=None):
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-
-    state_id = "State.{}".format(id)
-    if id is not None and state_id not in state_obj:
-        states = None
-    return render_template("9-states.html",
-                           states=states,
-                           cities=cities,
-                           id=id)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
